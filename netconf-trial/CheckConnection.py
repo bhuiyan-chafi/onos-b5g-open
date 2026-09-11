b53375c1ba4f7a6cc499e6134f7c68fd2e73fa0f
ASM CHAFIULLAH <a.bhuiyan@studenti.unipi.it>
2026-09-11 22:43:24 +0200

    restage all files

--- netconf-trial/CheckConnection.py
+++
@@ -1,7 +0,0 @@
-# save as hello.py
-from ncclient import manager
-m = manager.connect(host="10.100.101.11", port=2022, username="admin", password="admin",
-                    hostkey_verify=False, allow_agent=False, look_for_keys=False, timeout=10)
-print("Connected. Capabilities:", len(list(m.server_capabilities)))
-for c in m.server_capabilities:
-    print(" -", c)
